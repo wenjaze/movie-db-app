dda524cd3e8ae693a704aab04e0ed30d5f3f188c
rg <raska.gergely@gmail.com>
2020-08-04 21:19:13 +0200

    removed ALL unecessary imports

--- fragments/app/src/main/java/com/example/fragments/SecondFragment.kt
+++ fragments/app/src/main/java/com/example/fragments/SecondFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_second.view.*
-import kotlinx.android.synthetic.main.item_movie.view.*
 
 class SecondFragment : Fragment() {
     var movieTitle: String? = ""
