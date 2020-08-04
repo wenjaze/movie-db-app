2404f968a202ee91ee2a025bf9d42fd81a413a42
rg <raska.gergely@gmail.com>
2020-08-04 21:17:56 +0200

    removed unecessary imports

--- fragments/app/src/main/java/com/example/fragments/FirstFragment.kt
+++ fragments/app/src/main/java/com/example/fragments/FirstFragment.kt
@@ -1,12 +1,10 @@
 package com.example.fragments
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fragments.movie.Movie
