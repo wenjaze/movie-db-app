af8fc235be27ce7b432a398e723f56248346f935
rg <raska.gergely@gmail.com>
2020-08-06 00:06:46 +0200

    Added debouncing to searchField 500ms

--- fragments/app/src/main/java/com/example/fragments/SearchFragment.kt
+++ fragments/app/src/main/java/com/example/fragments/SearchFragment.kt
@@ -1,16 +1,23 @@
 package com.example.fragments
 
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fragments.movie.Movie
 import com.example.fragments.movie.MovieInflater
 import layout.MoviesAdapter
 import layout.onMovieItemClickListener
+import java.util.Timer
+import java.util.TimerTask
 
 class SearchFragment : Fragment(), onMovieItemClickListener {
 
@@ -24,6 +31,7 @@ class SearchFragment : Fragment(), onMovieItemClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         moviesList = MovieInflater.createTrialList()
         moviesAdapter = MoviesAdapter(moviesList, this)
+
         super.onCreate(savedInstanceState)
     }
 
@@ -33,6 +41,27 @@ class SearchFragment : Fragment(), onMovieItemClickListener {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_search, container, false)
         val rvMovies = rootView.findViewById<View>(R.id.moviesRecyclerView) as? RecyclerView
+        val searchField = rootView.findViewById<View>(R.id.searchField) as? EditText
+        searchField?.addTextChangedListener(
+            object : TextWatcher {
+                override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+                override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
+                private var timer: Timer = Timer()
+                private val DELAY: Long = 500
+                override fun afterTextChanged(s: Editable) {
+                    timer.cancel()
+                    timer = Timer()
+                    timer.schedule(object : TimerTask() {
+                        override fun run() {
+                            activity?.runOnUiThread {
+                                Toast.makeText(activity?.applicationContext, searchField.text, Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                    }, DELAY)
+                }
+            }
+        )
         rvMovies?.layoutManager = LinearLayoutManager(this.context)
         rvMovies?.adapter = moviesAdapter
         return rootView
@@ -54,4 +83,5 @@ class SearchFragment : Fragment(), onMovieItemClickListener {
         }
 
     }
+
 }
