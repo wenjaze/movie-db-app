a369faa6c87ddce266cbb206c0408919217d00f9
rg <raska.gergely@gmail.com>
2020-08-19 19:46:22 +0200

    :art: Formatted all codes

--- fragments/app/src/main/java/com/example/fragments/movie/JsonParser.kt
+++ fragments/app/src/main/java/com/example/fragments/movie/JsonParser.kt
@@ -7,32 +7,32 @@ import java.lang.Exception
 
 class JsonParser(val json: String) : Runnable {
 
-    private var movieResultsTotal: Int = 0
-    private var movieResultsList: List<MovieJson> = listOf()
+	private var movieResultsTotal: Int = 0
+	private var movieResultsList: List<MovieJson> = listOf()
 
-    fun buildObject() {
-        val moshi = Moshi.Builder().build()
-        val jsonAdapter: JsonAdapter<MovieResults> = moshi.adapter(MovieResults::class.java)
-        val movieResults = jsonAdapter.fromJson(json)!!
-        movieResultsTotal = movieResults.total_results
-        movieResultsList = movieResults.results
-    }
+	fun buildObject() {
+		val moshi = Moshi.Builder().build()
+		val jsonAdapter: JsonAdapter<MovieResults> = moshi.adapter(MovieResults::class.java)
+		val movieResults = jsonAdapter.fromJson(json)!!
+		movieResultsTotal = movieResults.total_results
+		movieResultsList = movieResults.results
+	}
 
-    override fun run() {
-        buildObject()
-    }
+	override fun run() {
+		buildObject()
+	}
 
-    fun createObject() {
-        Thread(Runnable {
-            try {
-                run()
-            } catch (e: Exception) {
-                Log.d("Error", "couldn't run task.")
-            }
-        }).run()
-    }
+	fun createObject() {
+		Thread(Runnable {
+			try {
+				run()
+			} catch (e: Exception) {
+				Log.d("Error", "couldn't run task.")
+			}
+		}).run()
+	}
 
-    fun getTotalResults() = movieResultsTotal
-    fun getResultList() = movieResultsList
+	fun getTotalResults() = movieResultsTotal
+	fun getResultList() = movieResultsList
 
 }
--- fragments/app/src/main/java/com/example/fragments/movie/MovieInflater.kt
+++ fragments/app/src/main/java/com/example/fragments/movie/MovieInflater.kt
@@ -21,11 +21,11 @@ object MovieInflater {
     fun getJsonDataFromAsset(context: Context, fileName: String): String? {
         val jsonString: String
         try {
-                jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
-            } catch (ioException: IOException) {
-                ioException.printStackTrace()
-                return null
-            }
-            return jsonString
+            jsonString = context.assets.open(fileName).bufferedReader().use { it.readText() }
+        } catch (ioException: IOException) {
+            ioException.printStackTrace()
+            return null
         }
+        return jsonString
+    }
 }
