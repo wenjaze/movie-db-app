53cb735f36b825524f8e72e07256ce46ce6fc1f3
rg <raska.gergely@gmail.com>
2020-08-25 19:52:57 +0200

    :art: Clean code

--- fragments/app/src/main/java/com/example/fragments/movie/MoviesAdapter.kt
+++ fragments/app/src/main/java/com/example/fragments/movie/MoviesAdapter.kt
@@ -8,40 +8,40 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.fragments.R
 
 class MoviesAdapter(private var movies: List<Movie>, var clickListener: onMovieItemClickListener) :
-    RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
+	RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val movieItemView = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
-        return ViewHolder(movieItemView)
-    }
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+		val movieItemView = LayoutInflater.from(parent.context).inflate(R.layout.item_movie, parent, false)
+		return ViewHolder(movieItemView)
+	}
 
-    override fun onBindViewHolder(viewholder: ViewHolder, position: Int) {
-        viewholder.init(movies[position], clickListener)
-    }
+	override fun onBindViewHolder(viewholder: ViewHolder, position: Int) {
+		viewholder.init(movies[position], clickListener)
+	}
 
-    override fun getItemCount(): Int = movies.size
+	override fun getItemCount(): Int = movies.size
 
-    interface onMovieItemClickListener {
-        fun onItemClick(item: Movie, position: Int)
-    }
+	interface onMovieItemClickListener {
+		fun onItemClick(item: Movie, position: Int)
+	}
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val movieTitleView = itemView.findViewById<TextView>(R.id.movieItem)
-        fun init(item: Movie, action: onMovieItemClickListener) {
-            movieTitleView.text = item.title
-            itemView.setOnClickListener() {
-                action.onItemClick(item, adapterPosition)
-            }
-        }
-    }
+	inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+		val movieTitleView = itemView.findViewById<TextView>(R.id.movieItem)
+		fun init(item: Movie, action: onMovieItemClickListener) {
+			movieTitleView.text = item.title
+			itemView.setOnClickListener() {
+				action.onItemClick(item, adapterPosition)
+			}
+		}
+	}
 
-    fun clearAdapter() {
-        movies = emptyList()
-        notifyDataSetChanged()
-    }
+	fun clearAdapter() {
+		movies = emptyList()
+		notifyDataSetChanged()
+	}
 
-    fun setMovies(mvs: List<Movie>) {
-        movies = mvs
-        notifyDataSetChanged()
-    }
+	fun setMovies(mvs: List<Movie>) {
+		movies = mvs
+		notifyDataSetChanged()
+	}
 }
