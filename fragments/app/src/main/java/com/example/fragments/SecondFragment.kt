7e93ea565629b3bbc4d115a4ceab8ce9e9b915ff
rg <raska.gergely@gmail.com>
2020-08-25 12:51:17 +0200

    :lipstick: Update UI : fragment_second.xml

--- fragments/app/src/main/java/com/example/fragments/SecondFragment.kt
+++ fragments/app/src/main/java/com/example/fragments/SecondFragment.kt
@@ -1,12 +1,14 @@
 package com.example.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.fragments.movie.Movie
-import kotlinx.android.synthetic.main.fragment_second.movie_id
+import kotlinx.android.synthetic.main.fragment_second.movie_overview
+import kotlinx.android.synthetic.main.fragment_second.movie_popularity
 import kotlinx.android.synthetic.main.fragment_second.movie_release_date
 import kotlinx.android.synthetic.main.fragment_second.movie_title
 import kotlinx.android.synthetic.main.fragment_second.movie_vote_average
@@ -19,12 +21,14 @@ class SecondFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_second, container, false)
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         arguments?.getParcelable<Movie>("keyData")?.run {
-            movie_id.text = id.toString()
-            movie_release_date.text = release_date
-            movie_title.text = title
-            movie_vote_average.text = vote_average.toString()
+            movie_overview.text = "overview:\n $overview"
+            movie_popularity.text = "popularity:\n $popularity"
+            movie_release_date.text = "release date:\n $release_date"
+            movie_title.text = "title:\n $title"
+            movie_vote_average.text = "vote_average:\n $vote_average"
         }
         super.onViewCreated(view, savedInstanceState)
     }
