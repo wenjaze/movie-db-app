ad1e40f09b9732eb20986e6a8e0892952d82c4b2
rg <raska.gergely@gmail.com>
2020-08-31 18:12:06 +0200

    :fire: Remove unnecessary Classes

--- fragments/app/src/main/java/com/example/fragments/movie/MovieJson.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fragments.movie
-
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class MovieJson(val id: Long, val vote_average: Double, val title: String, val release_date: Int)
